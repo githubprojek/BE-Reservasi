5d7c062b2052574e3fa2014530916e7baee82653
githubprojek <githubprojek@gmail.com>
2025-09-17 09:29:22 +0700

    ganti utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -8,7 +8,7 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+    sameSite: process.env.NODE_ENV === "None",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
