f9ce0df7f4cfefe7127f1aa1e60f035ebd0e510c
githubprojek <githubprojek@gmail.com>
2026-06-29 18:49:10 +0700

    fix: change token storage from cookies to local storage

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 export const protectRoute = async (req, res, next) => {
   try {
-    const token = req.cookies.jwt;
+    const token = req.headers.authorization?.split(" ")[1];
     if (!token) {
       return res.status(401).json({ message: "Unauthorized = No token provided" });
     }
--- src/routes/login.route.js
+++ src/routes/login.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import { login, register, logout, getUser, updateUser, deleteUser, getMe } from "../controllers/login.controllers.js";
 import { protectRoute, authorizeRoles } from "../middleware/auth.middleware.js";
 import { validateRegister, validateLogin, validateUpdateUser } from "../middleware/validation.login.js";
+import { response_success } from "../utils/response.js";
 const router = express.Router();
 
 router.post("/register", validateRegister, register);
@@ -13,9 +14,9 @@ router.delete("/deleteUser/:userId", deleteUser);
 router.get("/getMe", getMe);
 // router.post("/register", protectRoute, authorizeRoles("Super Admin"), register);
 // router.post("/login", login);
-// router.get("/check-auth", protectRoute, (req, res) => {
-//   res.status(200).json({ user: req.login });
-// });
+router.get("/check-auth", protectRoute, (req, res) => {
+  return response_success(res, { user: req.login }, "Authenticated");
+});
 // router.post("/logout", logout);
 // router.get("/getUser", protectRoute, getLogin);
 // router.put("/updateStaff/:staffId", protectRoute, authorizeRoles("Super Admin"), updateStaff);
